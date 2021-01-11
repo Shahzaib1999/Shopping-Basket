2ea37b802d117e954e48ecf26868f08ac6652734
M.Shahzaib <mohdshahzaib24@gmail.com>
2021-01-12 01:30:47 +0500

    implemented filter and sorting

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,21 +1,32 @@
 import { Row, Col, Input } from "reactstrap";
 
-import './SearchBar.css';
+import "./SearchBar.css";
 
-const SearchBar = () => (
-    <Row className="searchbar-wrapper pt-5">
-        <Col md={8}>
-            <Input type="text" placeholder="Search" style={{ width: "100%", border: 0, borderBottom: '1px solid', outline: 'none', borderColor: '#ced4da' }} />
-        </Col>
-        <Col md={4}>
-            <Input type="select">
-                <option value="desending">Date Descending</option>
-                <option value="desending">Date Asending</option>
-                <option value="desending">Price Descending</option>
-                <option value="desending">Price Asending</option>
-            </Input>
-        </Col>
-    </Row>
+interface Props {
+  onSearch: (e: String) => void;
+  onSort: (e: String) => void;
+}
+
+const SearchBar = ({ onSearch, onSort }: Props) => (
+  <Row className="searchbar-wrapper pt-5">
+    <Col md={8}>
+      <Input
+        type="text"
+        placeholder="Search"
+        className="search-input"
+        onChange={(e) => onSearch(e.target.value)}
+      />
+    </Col>
+    <Col md={4}>
+      <Input type="select" onChange={(e) => onSort(e.target.value)}>
+        <option value="">Sort</option>
+        <option value="priceascending">Price Low - High</option>
+        <option value="pricedesending">Price High - Low</option>
+        <option value="dateascending">Date Ascending</option>
+        <option value="datedesending">Date Descending</option>
+      </Input>
+    </Col>
+  </Row>
 );
 
 export default SearchBar;
--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,21 +1,78 @@
+import { useEffect, useState } from "react";
 import { Row, Col } from "reactstrap";
 import SearchBar from "../../components/SearchBar/SearchBar";
 
 import NavbarCom from "../../components/Navbar/Navbar";
 import Card from "../../components/Card/Card";
 import appartments from "../../Utils/appartments.json";
+import "./Home.css";
+
+interface Appartment {
+  id: String;
+  name: String;
+  image: String;
+  price: Number;
+  date: string;
+}
 
 const Home: React.FunctionComponent<any> = () => {
+  const [filteredData, setFilteredData] = useState<Appartment[]>([]);
+  const [data, setData] = useState<Appartment[]>([]);
+
+  useEffect(() => {
+    setData(appartments);
+    setFilteredData(appartments);
+  }, []);
+
+  const onSearch = (e: String) => {
+    let filterData = data.filter((l) => {
+      return l.name.toLowerCase().includes(e.toLowerCase());
+    });
+    setFilteredData(filterData);
+  };
+
+  const onSort = (e: String) => {
+    let sortedData;
+    switch (e) {
+      case "datedesending":
+        sortedData = [...data].sort(
+          (a, b) => new Date(b.date).valueOf() - new Date(a.date).valueOf()
+        );
+        setFilteredData(sortedData);
+        break;
+      case "dateascending":
+        sortedData = [...data].sort(
+          (a, b) => new Date(a.date).valueOf() - new Date(b.date).valueOf()
+        );
+        setFilteredData(sortedData);
+        break;
+      case "priceascending":
+        sortedData = [...data].sort((a, b) => +a.price - +b.price);
+        setFilteredData(sortedData);
+        break;
+      case "pricedesending":
+        sortedData = [...data].sort((a, b) => +b.price - +a.price);
+        setFilteredData(sortedData);
+        break;
+      default:
+        setFilteredData(data);
+        break;
+    }
+  };
+
   if (!appartments || !appartments.length) {
     return <h1>Loading</h1>;
   }
   return (
-    <div className="pb-5" style={{ background: "#ECEFF4" }}>
+    <div className="home-wrapper pb-5" style={{ background: "#ECEFF4" }}>
       <div className="container">
-        <SearchBar />
+        <SearchBar
+          onSearch={(e) => onSearch(e)}
+          onSort={(e: String) => onSort(e)}
+        />
         <Row className="mt-5">
-          {appartments.length &&
-            appartments.map((appartment, ind) => (
+          {filteredData.length &&
+            filteredData.map((appartment, ind) => (
               <Col key={ind} md={4} className="mt-4">
                 <Card appartment={appartment} />
               </Col>
